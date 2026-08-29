cb3c2c9ec7ea6798a9e93c153dc01d24606af427
root <root@localhost.localdomain>
2026-08-30 02:15:43 +1300

    Ajusta canciones populares y elimina Jesus Moreno

--- lib/data/canciones_populares.dart
+++ lib/data/canciones_populares.dart
@@ -1,189 +1,183 @@
 import '../cancion.dart';
-import '../lista_canciones.dart';
+import 'all_songs.dart';
 
 final List<Cancion> cancionesPopulares = [
   // Roberto Carballo
-  listaCanciones.firstWhere(
+  allSongs.firstWhere(
     (c) =>
         c.artista == "Roberto Carballo" &&
         c.titulo == "Aquí Está Roberto El Zorro",
   ),
-  listaCanciones.firstWhere(
+  allSongs.firstWhere(
     (c) =>
         c.artista == "Roberto Carballo" &&
         c.titulo == "Bonita",
   ),
 
-  // Jesús Moreno
-  listaCanciones.firstWhere(
-    (c) =>
-        c.artista == "Jesús Moreno" &&
-        c.titulo == "Amiga, Novia y Amante",
-  ),
 
   // Edwin Trocel
-  listaCanciones.firstWhere(
+  allSongs.firstWhere(
     (c) =>
         c.artista == "Edwin Trocel" &&
         c.titulo == "Himno de Victoria",
   ),
 
   // Francisco Camacho
-  listaCanciones.firstWhere(
+  allSongs.firstWhere(
     (c) =>
         c.artista == "Francisco Camacho" &&
         c.titulo == "Dos Amigos Compañeros",
   ),
 
   // Omar García
-  listaCanciones.firstWhere(
+  allSongs.firstWhere(
     (c) =>
         c.artista == "Omar García" &&
         c.titulo == "Soy De Cojedes",
   ),
 
   // Nicolás Castillo
-  listaCanciones.firstWhere(
+  allSongs.firstWhere(
     (c) =>
         c.artista == "Nicolás Castillo" &&
         c.titulo == "Traidor Destino",
   ),
 
   // Enrique De la Vega
-  listaCanciones.firstWhere(
+  allSongs.firstWhere(
     (c) =>
         c.artista == "Enrique De la Vega" &&
         c.titulo == "El Cantante de Planta",
   ),
 
   // José Ángel Cordero
-  listaCanciones.firstWhere(
+  allSongs.firstWhere(
     (c) =>
         c.artista == "José Ángel Cordero" &&
         c.titulo == "Mi Padre y Gran Amigo",
   ),
 
   // Mary Pacheco
-  listaCanciones.firstWhere(
+  allSongs.firstWhere(
     (c) =>
         c.artista == "Mary Pacheco" &&
         c.titulo == "Propiedad Privada",
   ),
 
   // Manuel Oliveros
-  listaCanciones.firstWhere(
+  allSongs.firstWhere(
     (c) =>
         c.artista == "Manuel Oliveros" &&
         c.titulo == "El Maquillaje",
   ),
 
   // Fabiana Díaz
-  listaCanciones.firstWhere(
+  allSongs.firstWhere(
     (c) =>
         c.artista == "Fabiana Díaz" &&
         c.titulo == "Proyecto Divino",
   ),
 
   // Tatiana López
-  listaCanciones.firstWhere(
+  allSongs.firstWhere(
     (c) =>
         c.artista == "Tatiana López" &&
         c.titulo == "Tu Mirada Es Una Soga",
   ),
 
   // José Pérez
-  listaCanciones.firstWhere(
+  allSongs.firstWhere(
     (c) =>
         c.artista == "José Pérez" &&
         c.titulo == "De a Caballo y Toro Solo",
   ),
 
   // Rafael Cordero
-  listaCanciones.firstWhere(
+  allSongs.firstWhere(
     (c) =>
         c.artista == "Rafael Cordero" &&
         c.titulo == "Yo Sigo Siendo Igualito",
   ),
 
   // José Mendoza
-  listaCanciones.firstWhere(
+  allSongs.firstWhere(
     (c) =>
         c.artista == "José Mendoza" &&
         c.titulo == "Amor Clandestino",
   ),
 
   // Juan Mendoza
-  listaCanciones.firstWhere(
+  allSongs.firstWhere(
     (c) =>
         c.artista == "Juan Mendoza" &&
         c.titulo == "Recogiendo la Maleta",
   ),
 
   // Jesús Alejandro
-  listaCanciones.firstWhere(
+  allSongs.firstWhere(
     (c) =>
         c.artista == "Jesús Alejandro" &&
         c.titulo == "Vivir Amándote",
   ),
 
   // Mercedes Guevara
-  listaCanciones.firstWhere(
+  allSongs.firstWhere(
     (c) =>
         c.artista == "Mercedes Guevara" &&
         c.titulo == "Cuando Se Acabe Tu Suerte",
   ),
 
   // Itsia Bastidas
-  listaCanciones.firstWhere(
+  allSongs.firstWhere(
     (c) =>
         c.artista == "Itsia Bastidas" &&
         c.titulo == "Conmigo No Tiene Chance",
   ),
 
   // Luis Omar Escalona
-  listaCanciones.firstWhere(
+  allSongs.firstWhere(
     (c) =>
         c.artista == "Luis Omar Escalona" &&
         c.titulo == "Diez Mil Grafitis",
   ),
 
   // Rafael Pérez
-  listaCanciones.firstWhere(
+  allSongs.firstWhere(
     (c) =>
         c.artista == "Rafael Pérez" &&
         c.titulo == "Tronco E Vaina",
   ),
 
   // Juan Herrera
-  listaCanciones.firstWhere(
+  allSongs.firstWhere(
     (c) =>
         c.artista == "Juan Herrera" &&
         c.titulo == "Mañana Empañeto el Rancho",
   ),
 
   // Wilmer Tovar
-  listaCanciones.firstWhere(
+  allSongs.firstWhere(
     (c) =>
         c.artista == "Wilmer Tovar" &&
         c.titulo == "Amor de Infancia",
   ),
 
   // Miguelito Díaz
-  listaCanciones.firstWhere(
+  allSongs.firstWhere(
     (c) =>
         c.artista == "Miguelito Díaz" &&
         c.titulo == "Recoleando un Nuevo Amor",
   ),
 
   // Yohan Casadiego
-  listaCanciones.firstWhere(
+  allSongs.firstWhere(
     (c) =>
         c.artista == "Yohan Casadiego" &&
         c.titulo == "Recuerdos de un Pijotero",
   ),
 
   // Milena Benítez
-  listaCanciones.firstWhere(
+  allSongs.firstWhere(
     (c) =>
         c.artista == "Milena Benítez" &&
         c.titulo == "Por Dios, la Vida y la Suerte",
